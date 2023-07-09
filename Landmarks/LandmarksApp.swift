103e84192db3e141f8f030a6ebdabfc71a6f32fd
Mihail Kulik <mkulik2407@gmail.com>
2023-07-09 22:51:19 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mihail Kulik on 9.07.23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
