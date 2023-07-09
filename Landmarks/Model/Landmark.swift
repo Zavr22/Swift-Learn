3718f898d9e4d37866ba616acfaa79964af47a2a
Mihail Kulik <mkulik2407@gmail.com>
2023-07-09 23:45:20 +0300

    feat: init project, first views created

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,32 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Mihail Kulik on 9.07.23.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+       var image: Image {
+           Image(imageName)
+       }
+    
+    private var coordinates: Coordinates
+
+
+        struct Coordinates: Hashable, Codable {
+            var latitude: Double
+            var longitude: Double
+        }
+}
+
